0ac09cc893be8c120fec336093740f6677be43fc
张尚斌 <zhangshangbin01@spic.com.cn>
2024-05-09 20:53:16 +0800

    feat: 获取个人页面内容

--- src/main/java/top/vicat/halo/plugin/gameposter/platforms/steam/SteamPlatformServiceImpl.java
+++ src/main/java/top/vicat/halo/plugin/gameposter/platforms/steam/SteamPlatformServiceImpl.java
@@ -3,9 +3,12 @@ package top.vicat.halo.plugin.gameposter.platforms.steam;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.TimeoutException;
+import java.util.regex.Pattern;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import top.vicat.halo.plugin.gameposter.entity.UserBaseProfile;
@@ -22,13 +25,16 @@ public class SteamPlatformServiceImpl implements IPlatformService {
     private final SteamApiClient steamApiClient;
     private final SteamProfileClient steamProfileClient;
 
-    private record PlayerStateRecord(
-        PlayerState playerState,
-        UserBaseProfile.UserBaseProfileSpec userBaseProfile
-    ) {}
+    private static final String AVATAR_MASK_XPATH = "//*[@id=\"responsive_page_template_content\"]/div/div[1]/div/div/div/div[2]/div/div/img";
+    private static final String BACKGROUND_XPATH = "//*[@id=\"responsive_page_template_content\"]/div";
+    private static final Pattern backgroundPattern = Pattern.compile("background-image\\s*:\\s*url\\(\\s*'([^']+)'\\s*\\)");
 
     @Override
     public Mono<UserBaseProfile.UserBaseProfileSpec> getUserBaseProfile(String accountId) {
+        record PlayerStateRecord(
+            PlayerState playerState,
+            UserBaseProfile.UserBaseProfileSpec userBaseProfile
+        ) {}
         Mono<PlayerState> playerStateMono =
             steamApiClient.getUserState(Collections.singletonList(accountId)).next();
         return playerStateMono.map(playerState -> {
@@ -51,6 +57,19 @@ public class SteamPlatformServiceImpl implements IPlatformService {
         }).flatMap(playerStateRecord -> steamProfileClient.getProfile(playerStateRecord.playerState().profileUrl())
                 .map(document -> {
                     System.out.println(document);
+                    Optional<Document> docOpt = Optional.ofNullable(document);
+                    docOpt.map(doc -> doc.selectXpath(AVATAR_MASK_XPATH).first())
+                        .map(element -> element.attr("src"))
+                            .ifPresent(resource -> playerStateRecord.userBaseProfile().setAvatarMask(new UserBaseProfile.ProfileMedia(
+                                resource,
+                                UserBaseProfile.ProfileMediaType.IMAGE
+                            )));
+                    docOpt.map(doc -> doc.selectXpath(BACKGROUND_XPATH).first())
+                            .map(element -> element.attr("style"))
+                                .ifPresent(style -> {
+                                    String group = backgroundPattern.matcher(style).group(1);
+                                    log.info(group);
+                                });
                     playerStateRecord.userBaseProfile().setExtra("xxxxxx");
                     return playerStateRecord.userBaseProfile();
                 }).timeout(Duration.ofSeconds(5))
